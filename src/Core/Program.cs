f19d0843a9b2ab9d4d07e0bd8052b5541777b0df
Boomerxl <157655489+Boomerxl@users.noreply.github.com>
2026-03-10 15:05:50 -0400

    Initial project skeleton: folders and Program.cs placeholder

---
+++ src/Core/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TalosForge.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TalosForge initializing... Forge the guardian!");
+        }
+    }
+}
